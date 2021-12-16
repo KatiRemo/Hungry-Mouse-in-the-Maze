3b9187889a1ea5da02adfa6b1bcbc47d59e5edd6
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-12-16 12:56:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import Maze from './components/Maze';
+// import Cell from './components/Cell';
+// import Path from './components/Path';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const App = () => {
+//     return (
+//         <div>
+//             <Maze />
+//             <Cell />
+//             <Path />
+//         </div>
+//     );
+// };
 
-export default App;
+// export default App;
